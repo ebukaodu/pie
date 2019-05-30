dc88ee922db24cd6d9f3b10cd1cfc12fc604537b
ebukaodu <chukas4christ@gmail.com>
2019-05-30 16:12:57 -0600

    Add files via upload

---
+++ Pie_Pizzeria_2-Razor_Page/Models/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PiePizzeria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PiePizzeria.Models
+{
+    public class HomeViewModel
+    {
+        public List<Pie> Pies { get; set; }
+        public string Title { get; set; }
+    }
+}
